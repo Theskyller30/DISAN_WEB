c9b0b05211175719ebc434c066552b939265cdc4
Juan Garcia <118154686+Theskyller30@users.noreply.github.com>
2023-09-20 09:50:18 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,15 +1,17 @@
-const loginForm = document.querySelector('#loginForm')
-loginForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = Users.find(user => user.email === email && user.password === password)
-    if(!validUser){
-        return alert('Usuario y/o contraseña incorrectos!')
-    }
-    alert(`Bienvenido ${validUser.name}`)
-    localStorage.setItem('login_success', JSON.stringify(validUser))
-    window.location.href = 'index.html'   
+const loginForm = document.querySelector('#loginForm');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
 
-})
+    // Verificar si el correo y la contraseña coinciden con los valores permitidos
+    if (email === 'Disan@serpomar.com' && password === 'Prueba123') {
+        // Usuario válido, redirige a la página de inicio
+        alert('Bienvenido Disan');
+        localStorage.setItem('login_success', JSON.stringify({ email: email }));
+        window.location.href = 'index.html';
+    } else {
+        // Usuario no válido
+        alert('Usuario y/o contraseña incorrectos!');
+    }
+});
